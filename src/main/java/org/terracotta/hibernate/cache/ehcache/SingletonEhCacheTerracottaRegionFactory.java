1731a606c6969c3d323e6e27a701913c1377a763
Fabien Sanglier <fabiensanglier@gmail.com>
2015-05-28 13:28:11 -0400

    initial commit

---
+++ src/main/java/org/terracotta/hibernate/cache/ehcache/SingletonEhCacheTerracottaRegionFactory.java
@@ -0,0 +1,124 @@
+package org.terracotta.hibernate.cache.ehcache;
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.config.Configuration;
+import org.hibernate.cache.CacheException;
+import org.hibernate.cache.ehcache.EhCacheMessageLogger;
+import org.hibernate.cfg.Settings;
+import org.jboss.logging.Logger;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * A singleton EhCacheRegionFactory implementation.
+ *
+ * @author Chris Dennis
+ * @author Greg Luck
+ * @author Emmanuel Bernard
+ * @author Alex Snaps
+ * @author Fabien Sanglier - Modified to extend the CustomAbstractEhcacheRegionFactory class and use the CustomHibernateEhcacheUtils class
+ */
+public class SingletonEhCacheTerracottaRegionFactory extends CustomAbstractEhcacheRegionFactory {
+    private static final EhCacheMessageLogger LOG = Logger.getMessageLogger(
+            EhCacheMessageLogger.class,
+            SingletonEhCacheTerracottaRegionFactory.class.getName()
+    );
+
+    private static final AtomicInteger REFERENCE_COUNT = new AtomicInteger();
+
+    /**
+     * Constructs a SingletonEhCacheRegionFactory
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    public SingletonEhCacheTerracottaRegionFactory() {
+    }
+
+    /**
+     * Constructs a SingletonEhCacheRegionFactory
+     *
+     * @param prop Not used
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    public SingletonEhCacheTerracottaRegionFactory(Properties prop) {
+        super();
+    }
+
+    @Override
+    public void start(Settings settings, Properties properties) throws CacheException {
+        this.settings = settings;
+        try {
+            String configurationResourceName = null;
+            if ( properties != null ) {
+                configurationResourceName = (String) properties.get( NET_SF_EHCACHE_CONFIGURATION_RESOURCE_NAME );
+            }
+            if ( configurationResourceName == null || configurationResourceName.length() == 0 ) {
+                manager = CacheManager.create();
+                REFERENCE_COUNT.incrementAndGet();
+            }
+            else {
+                URL url;
+                try {
+                    url = new URL( configurationResourceName );
+                }
+                catch (MalformedURLException e) {
+                    if ( !configurationResourceName.startsWith( "/" ) ) {
+                        configurationResourceName = "/" + configurationResourceName;
+                        LOG.debugf(
+                                "prepending / to %s. It should be placed in the root of the classpath rather than in a package.",
+                                configurationResourceName
+                        );
+                    }
+                    url = loadResource( configurationResourceName );
+                }
+                final Configuration configuration = HibernateEhcacheTerracottaUtils.loadAndCorrectConfiguration(url);
+                manager = CacheManager.create(configuration);
+                REFERENCE_COUNT.incrementAndGet();
+            }
+            mbeanRegistrationHelper.registerMBean( manager, properties );
+        }
+        catch (net.sf.ehcache.CacheException e) {
+            throw new CacheException( e );
+        }
+    }
+
+    @Override
+    public void stop() {
+        try {
+            if ( manager != null ) {
+                if ( REFERENCE_COUNT.decrementAndGet() == 0 ) {
+                    manager.shutdown();
+                }
+                manager = null;
+            }
+        }
+        catch (net.sf.ehcache.CacheException e) {
+            throw new CacheException( e );
+        }
+    }
+}
